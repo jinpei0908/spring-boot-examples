343979995b38d8bd00b7bed9179f434e64d2eb49
jinpei0908 <jinpei0908@gmail.com>
2022-04-20 21:53:14 +0900

    PathVariableのサンプルを追加した

--- validation/src/main/java/com/example/validation/exmaple/controller/ValidationExampleController.java
+++ validation/src/main/java/com/example/validation/exmaple/controller/ValidationExampleController.java
@@ -5,18 +5,24 @@ import com.example.validation.exmaple.factory.ValidationErrorMessageFactory;
 import com.example.validation.exmaple.validator.UserValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.Pattern;
 
-@Slf4j
-@RestController
-@RequiredArgsConstructor
+
 @RequestMapping(value = "v1")
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+@Validated
 public class ValidationExampleController {
 
     private final ValidationErrorMessageFactory errorMessageFactory;
@@ -34,6 +40,13 @@ public class ValidationExampleController {
     @GetMapping(value = "complicated-users")
     public User getUser(@ModelAttribute User user) {
         userValidator.validate(user);
+        return new User();
+    }
+
+    @GetMapping(value = "path-variable/{id}/{name}")
+    public User getUser(
+            @PathVariable @Range(min = 1, max = 1_000_000_000L) Long id,
+            @PathVariable @Pattern(regexp = "[A-z]+") String name) {
         return new User();
     }
 }
